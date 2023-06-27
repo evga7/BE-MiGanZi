2af8adbc80d73f96ebcd34ea2311ecaef4fb141a
blue <evga7@naver.com>
2023-06-27 20:35:08 +0900

    23.6.27 1. properties edit 2. config test, time zone test,jwt test

--- src/main/java/com/StreetNo5/StreetNo5/config/SecurityConfig.java
+++ src/main/java/com/StreetNo5/StreetNo5/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
                 .formLogin().disable()
                 .httpBasic().disable()
                 .authorizeHttpRequests()
-                .requestMatchers ("/user/**","/board/**","/swagger-ui/**", "/v3/api-docs/**").permitAll ()
+                .requestMatchers ("/user/**","/board","/swagger-ui/**", "/v3/api-docs/**").permitAll ()
                 //.requestMatchers("/board").hasRole("USER")
                 .and()
                 .cors().configurationSource(corsConfigurationSource())
