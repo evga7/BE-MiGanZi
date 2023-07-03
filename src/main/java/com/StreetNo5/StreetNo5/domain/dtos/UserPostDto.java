796fa203848ddbb3b64354a79dd1bb24a27ad965
blue <evga7@naver.com>
2023-07-03 13:47:27 +0900

    #6 edit popular post data transfer object
    23.6.27
    1. properties edit
    2. config test, time zone test,jwt test
    23.6.28
    1. add post with map info(tags) domain
    2. add get user post from user position test
    3. add password when user signup and login
    4. add decode from jwt and extract nickname
    23.6.29
    5. add gcp file upload
    6. return comments when a user replies
    23.7.1
    7. add tags_num
    8. #3 Return Posts Convert data to DTO
    9.hide pageable
    10. #5 edit api

--- src/main/java/com/StreetNo5/StreetNo5/domain/dtos/UserPostDto.java
+++ src/main/java/com/StreetNo5/StreetNo5/domain/dtos/UserPostDto.java
@@ -1,25 +1,20 @@
 package com.StreetNo5.StreetNo5.domain.dtos;
 
 
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.List;
 
 @Getter
 @Setter
 public class UserPostDto {
     private Long id;
-    @Size(min = 2,max = 30,message = "제목을 2~30자 사이로 입력해주세요.")
     private String nickname;
-    @Size(min = 2, max = 250,message = "내용은 2~500자 사이로 입력해주세요.")
+    private Long viewCount=0L;
     private String content;
     private String imageUrl;
-    private String createdAt;
-    private String modifiedAt;
-    private List<UserPostCommentDto> comments;
     private String address_name;
+    private String tags;
+    private String modifiedDate;
 
 
 }
