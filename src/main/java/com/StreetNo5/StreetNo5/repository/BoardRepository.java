8b29441b63da1ad215e5a5d22f56b9fffbd2defa
blue <evga7@naver.com>
2023-08-31 22:34:07 +0900

    -Fix Close #79 인기 게시물 조회 N+1 문제 해결 (쿼리 수정)

--- src/main/java/com/StreetNo5/StreetNo5/repository/BoardRepository.java
+++ src/main/java/com/StreetNo5/StreetNo5/repository/BoardRepository.java
@@ -15,7 +15,7 @@ public interface BoardRepository extends JpaRepository<UserPost,Long> {
     @Query(value = "update user_post p set p.view_count = p.view_count + 1 where p.post_id=:id",nativeQuery = true)
     void updatePageView(@Param("id") Long id);
 
-    @Query(value = "select * from user_post order by view_count desc limit 5",nativeQuery = true)
+    @Query(value = "select u from UserPost u left join fetch u.user order by u.viewCount desc limit 5")
     List<UserPost> findPolarPost();
 
     @Query(value = "select u from UserPost u left join fetch u.user" )
